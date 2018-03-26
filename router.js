25fa332656631aba2937c608db0eab670edd121b
xuanya <982798437@qq.com>
2018-03-26 22:03:49 +0800

    设计路由表&提取多个函数

--- router.js
+++ router.js
@@ -1,10 +1,27 @@
-const express=require('express')
-const router=express.Router()
+const express = require('express')
+const index = require('./controllers/index')
+const topic = require('./controllers/topic')
+const user = require('./controllers/user')
+const router = express.Router()
+
+//首页路由
 router
-    .get('/',(req,res)=>{
-        res.send('渲染首页')
-    })
+    .get('/', index.showIndex)
+    //用户路由
+router    
+    .get('/signin', user.showSignin)
+    .post('./signin', user.signin)
+    .get('/signup', user.showSignup)
+    .post('./signup', user.signup)
+    .post('./signout', user.signout)
+    // 话题路由
+router
+    .get('/signin', user.showSignin)
+    .post('./signin', user.signin)
+    .get('/signup', user.showSignup)
+    .post('./signup', user.signup)
+    .post('./signout', user.signout)
 
 
 
-    module.exports=router
+module.exports = router
